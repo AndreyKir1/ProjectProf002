b0007662a1dc176eb8a6c5751f8f2b570f2b9744
AndreyKir1 <smite@i.ua>
2017-08-14 14:15:22 +0200

    update 14.08.2017 ( order )

--- Homework/src/firma/fx/controllers/sales_manager/ChoseCustomer.java
+++ Homework/src/firma/fx/controllers/sales_manager/ChoseCustomer.java
@@ -1,13 +1,32 @@
 package firma.fx.controllers.sales_manager;
 
 import firma.hibernate.entity.Client;
+import firma.hibernate.service.client.ClientService;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+import java.text.ParseException;
 
 public class ChoseCustomer {
+
+    private ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"firma/Config.xml"});
+    private ClientService clientService = context.getBean(ClientService.class);
+
+    @FXML
+    private static ObservableList<Client> listClient;
+    private static Client currentClient;
 
     @FXML
     private Button btnCancel;
@@ -25,10 +44,10 @@ public class ChoseCustomer {
     private Button btnUpdate;
 
     @FXML
-    private TableView<Client> tblView;
+    private Label lblCustomerCount;
 
     @FXML
-    private Label lblCustomerCount;
+    private TableView<Client> tblView;
 
     @FXML
     private TableColumn<Client, String> columnLastName;
@@ -46,17 +65,66 @@ public class ChoseCustomer {
     private TableColumn<Client, String> columnName;
 
     @FXML
-    void pressAdd() {
-
+    void pressAdd() throws ParseException {
+        try {
+            Stage stage = new Stage();
+            stage.setTitle("Додати нового клієнта");
+            Parent root = FXMLLoader.load(getClass().getResource("/firma/view/sales_manager/AddUpdateCustomer.fxml"));
+            Scene scene = new Scene(root);
+            stage.setMinWidth(315);
+            stage.setMinHeight(360);
+            stage.setMaxWidth(415);
+            stage.setMaxHeight(360);
+            stage.setScene(scene);
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(btnAdd.getScene().getWindow());
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void pressUpdate() {
-
+        currentClient = tblView.getSelectionModel().getSelectedItem();
+        if (currentClient != null){
+            try {
+                Stage stage = new Stage();
+                stage.setTitle("Редагувати дані клієнта");
+                Parent root = FXMLLoader.load(getClass().getResource("/firma/view/sales_manager/AddUpdateCustomer.fxml"));
+                Scene scene = new Scene(root);
+                stage.setMinWidth(315);
+                stage.setMinHeight(500);
+                stage.setMaxWidth(415);
+                stage.setMaxHeight(500);
+                stage.setScene(scene);
+                stage.initModality(Modality.WINDOW_MODAL);
+                stage.initOwner(btnUpdate.getScene().getWindow());
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @FXML
     void pressDelete() {
+        currentClient = tblView.getSelectionModel().getSelectedItem();
+        if (currentClient != null){
+            try {
+                Stage stage = new Stage();
+                stage.setTitle("Видалення клієнта");
+                Parent root = FXMLLoader.load(getClass().getResource("/firma/view/sales_manager/DeleteCustomerConfirm.fxml"));
+                Scene scene = new Scene(root);
+                stage.setResizable(false);
+                stage.setScene(scene);
+                stage.initModality(Modality.WINDOW_MODAL);
+                stage.initOwner(btnDelete.getScene().getWindow());
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 
